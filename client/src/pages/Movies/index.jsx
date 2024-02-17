c407c5f95f4f996fb933192fe9ca2e48e342bc2c
Ahmad Baghirov <tu5r38eh9@code.edu.az>
2024-02-17 16:05:01 +0400

    Filter added

--- client/src/pages/Movies/index.jsx
+++ client/src/pages/Movies/index.jsx
@@ -5,6 +5,10 @@ import Footer from '../../layout/Footer';
 import StarRating from '../../components/StarRating';
 import { Link } from 'react-router-dom';
 import './index.scss';
+import { FaArrowUp } from "react-icons/fa";
+import { FaArrowDown } from "react-icons/fa";
+import { FaFilter } from "react-icons/fa";
+import { Slider } from "antd"
 
 const MoviesPage = () => {
     const [data, setData] = useState([]);
@@ -12,6 +16,16 @@ const MoviesPage = () => {
     const [itemsPerPage] = useState(6);
     const { darkmode } = useContext(DarkmodeContext);
     const [search, setSearch] = useState('');
+    const [sort, setSort] = useState(null)
+    const [isFilterOpen, setIsFilterOpen] = useState(false)
+    const [selectedRatings, setSelectedRatings] = useState([]);
+    const [imdbRange, setImdbRange] = useState([0, 10]);
+    const [selectedCategories, setSelectedCategories] = useState([]);
+    const [catData, setCatData] = useState([])
+
+    const openFilter = () => {
+        setIsFilterOpen(!isFilterOpen)
+    }
 
     const fetchData = async () => {
         try {
@@ -23,16 +37,70 @@ const MoviesPage = () => {
         }
     };
 
+    const fetchCategories = async () => {
+        try {
+            const res = await fetch('http://localhost:5500/api/categories/');
+            const jsonData = await res.json();
+            setCatData(jsonData);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
     useEffect(() => {
         fetchData();
+        fetchCategories()
     }, []);
+
+
+    const handleRatingFilter = (rating) => {
+        if (selectedRatings.includes(rating)) {
+            setSelectedRatings(selectedRatings.filter((selected) => selected !== rating));
+        } else {
+            setSelectedRatings([...selectedRatings, rating]);
+        }
+    };
+
+    const filterByRating = (movie) => {
+        const averageRating = movie.ratings
+            ? movie.ratings.reduce((total, rating) => total + rating.rating, 0) / movie.ratings.length
+            : 0;
+
+        const imdbInRange = imdbRange[0] <= movie.imdb && movie.imdb <= imdbRange[1];
+
+        return (
+            (selectedRatings.length === 0 || selectedRatings.includes(Math.round(averageRating))) &&
+            imdbInRange
+        );
+    };
+
+    const handleCategoryFilter = (category) => {
+        if (selectedCategories.includes(category)) {
+            setSelectedCategories(selectedCategories.filter((selected) => selected !== category));
+        } else {
+            setSelectedCategories([...selectedCategories, category]);
+        }
+    };
+
+    const filterByCategory = (movie) => {
+        return (
+            selectedCategories.length === 0 ||
+            movie.category.some((cat) => selectedCategories.includes(cat.categoryname))
+        );
+    };
+
 
     const indexOfLastItem = currentPage * itemsPerPage;
     const indexOfFirstItem = indexOfLastItem - itemsPerPage;
     const filteredItems = data.filter((item) =>
         item.name.toLowerCase().trim().includes(search.toLowerCase())
     );
-    const currentItems = filteredItems.slice(indexOfFirstItem, indexOfLastItem);
+    const currentItems = filteredItems
+        .filter(filterByCategory)
+        .filter(filterByRating)
+        .slice(indexOfFirstItem, indexOfLastItem);
+
+
 
     const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
@@ -60,38 +128,102 @@ const MoviesPage = () => {
                         </label>
                     </div>
                     <div className='sort'>
-                        <button>a-z</button>
-                        <button>z-a</button>
-                        <button onClick={() => handleSort('1-9imdb')}>1-9imdb</button>
-                        <button onClick={() => handleSort('9-1imdb')}>9-1imdb</button>
+                        <button onClick={() => setSort({ property: "name", asc: true })}>A-Z</button>
+                        <button onClick={() => setSort({ property: "name", asc: false })}>Z-A</button>
+                        <button onClick={() => setSort(null)}>Default</button>
+                        <button onClick={() => setSort('1-9imdb')}>IMDb <FaArrowUp /></button>
+                        <button onClick={() => setSort('9-1imdb')}>IMDb <FaArrowDown /></button>
+                    </div>
+                    <div className="filter">
+                        <button onClick={openFilter}>Filter <FaFilter /></button>
+                    </div>
+                </div>
+                <div className={isFilterOpen ? "filtered" : "displaynone"}>
+                    <div className="left">
+                        <div className='byrating'>
+                            {[5, 4, 3, 2, 1].map((rating) => (
+                                <span key={rating}>
+                                    <label htmlFor={`rating-${rating}`}>
+                                        <StarRating rating={rating} />
+                                    </label>
+                                    <input
+                                        type='checkbox'
+                                        name={`rating-${rating}`}
+                                        id={`rating-${rating}`}
+                                        checked={selectedRatings.includes(rating)}
+                                        onChange={() => handleRatingFilter(rating)}
+                                    />
+                                </span>
+                            ))}
+                        </div>
+                        <div className="byimdb">
+                            <Slider
+                                range
+                                max={10}
+                                value={imdbRange}
+                                onChange={(value) => setImdbRange(value)}
+                            />
+
+                        </div>
+                    </div>
+                    <div className="bycategory">
+                        {catData.map((category) => (
+                            <span key={category._id}>
+                                <label htmlFor={`category-${category.categoryname}`}>
+                                    {category.categoryname}
+                                </label>
+                                <input
+                                    type='checkbox'
+                                    name={`category-${category.categoryname}`}
+                                    id={`category-${category.categoryname}`}
+                                    checked={selectedCategories.includes(category.categoryname)}
+                                    onChange={() => handleCategoryFilter(category.categoryname)}
+                                />
+                            </span>
+                        ))}
                     </div>
                 </div>
                 <div className={`movies ${darkmode ? 'darkmovie' : 'lightmovie'}`}>
                     {currentItems &&
-                        currentItems.map((item) => {
-                            const averageRating = item.ratings
-                                ? item.ratings.reduce((total, rating) => total + rating.rating, 0) / item.ratings.length
-                                : 0;
+                        currentItems
+                            .filter(filterByCategory)
+                            .sort((a, b) => {
+                                if (sort && sort.asc === true) {
+                                    return a[sort.property] > b[sort.property] ? 1 : b[sort.property] > a[sort.property] ? -1 : 0;
+                                } else if (sort && sort.asc === false) {
+                                    return a[sort.property] < b[sort.property] ? 1 : b[sort.property] < a[sort.property] ? -1 : 0;
+                                } else if (sort === '1-9imdb') {
+                                    return a.imdb > b.imdb ? 1 : b.imdb > a.imdb ? -1 : 0;
+                                } else if (sort === '9-1imdb') {
+                                    return a.imdb < b.imdb ? 1 : b.imdb < a.imdb ? -1 : 0;
+                                } else {
+                                    return 0;
+                                }
+                            })
+                            .map((item) => {
+                                const averageRating = item.ratings
+                                    ? item.ratings.reduce((total, rating) => total + rating.rating, 0) / item.ratings.length
+                                    : 0;
 
-                            return (
-                                <Link to={`/detail/${item._id}`} key={item.id}>
-                                    <ul>
-                                        <li className='imgli'>
-                                            <img src={item.image} alt='' />
-                                        </li>
-                                        <li>{item.name}</li>
-                                        <li>Language: {item.lang}</li>
-                                        <li>
-                                            {item.ratings && item.ratings.length > 0 ? (
-                                                <StarRating rating={averageRating} />
-                                            ) : (
-                                                <StarRating rating={averageRating} />
-                                            )}
-                                        </li>
-                                    </ul>
-                                </Link>
-                            );
-                        })}
+                                return (
+                                    <Link to={`/detail/${item._id}`} key={item.id}>
+                                        <ul>
+                                            <li className='imgli'>
+                                                <img src={item.image} alt='' />
+                                            </li>
+                                            <li>{item.name}</li>
+                                            <li>Language: {item.lang}</li>
+                                            <li>
+                                                {item.ratings && item.ratings.length > 0 ? (
+                                                    <StarRating rating={averageRating} />
+                                                ) : (
+                                                    <StarRating rating={averageRating} />
+                                                )}
+                                            </li>
+                                        </ul>
+                                    </Link>
+                                );
+                            })}
                 </div>
                 <div className='pagination'>
                     {filteredItems.length > itemsPerPage && (
